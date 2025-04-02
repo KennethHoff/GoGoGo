4be96e6ffcb0ea4936e8f86b3406fe5c0bc45ffa
Kenneth Hoff <kenneth.98@hotmail.com>
2025-04-02 16:13:14 +0200

    Create Flake.nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+          ];
+        };
+      }
+    );
+}
